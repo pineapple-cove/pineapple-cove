9d30232d3bcfc1d59e53aec1c6c7c052b08b2c4c
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2023-09-11 21:50:46 -0500

    Added new generated menu button class.

---
+++ src/Source/PineappleCove/Ui/MenuButton.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MenuButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINEAPPLECOVE_API UMenuButton : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
